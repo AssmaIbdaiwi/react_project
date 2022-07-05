9061434e50117fe506ee93cee9cc74f036aa79f0
AssmaIbdaiwi <100359495+AssmaIbdaiwi@users.noreply.github.com>
2022-07-05 17:58:05 +0300

    main

--- booking_hotel/src/App.js
+++ booking_hotel/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './style/css/bootstrap.css';
+import './style/vendors/linericon/style.css';
+import './style/css/font-awesome.min.css';
+import './style/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css';
+import './style/vendors/nice-select/css/nice-select.css';
+// import '../../style/vendors/owl-carousel/owl.carousel.min.css';
+import './style/vendors/lightbox/simpleLightbox.css';
+import './style/css/style.css';
+import './style/css/responsive.css';
+import ViewRoom from "./components/ViewRoom";
 function App() {
   return (
     <div className="App">
-        
+       <ViewRoom/> 
     </div>
   );
 }
---
+++ booking_hotel/src/components/ViewRoom.js
@@ -0,0 +1,76 @@
+
+// import '../../style/css/bootstrap.css';
+// import '../../style/vendors/linericon/style.css';
+// import '../../style/css/font-awesome.min.css';
+// import '../../style/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css';
+// import '../../style/vendors/nice-select/css/nice-select.css';
+// import '../../style/vendors/owl-carousel/owl.carousel.min.css';
+// import '../../style/vendors/lightbox/simpleLightbox.css';
+// import '../../style/css/style.css';
+// import '../../style/css/responsive.css';
+// js
+
+import  React from 'react';
+
+const ViewRoom = () => {
+    return ( <>
+        <header class="header_area">
+            <div class="">
+                <nav class="navbar navbar-expand-lg navbar-light">
+      
+                    <a class="navbar-brand logo_h" href="index.html"><img src="image/Logo.png" alt=""/></a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+           
+                    <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+                        <ul class="nav navbar-nav menu_nav ml-auto">
+                            <li class="nav-item active"><a class="nav-link" href="index.html">Home</a></li> 
+                            <li class="nav-item"><a class="nav-link" href="about.html">About us</a></li>
+                            <li class="nav-item"><a class="nav-link" href="accomodation.html">Accomodation</a></li>
+                            <li class="nav-item"><a class="nav-link" href="gallery.html">Gallery</a></li>
+                            <li class="nav-item submenu dropdown">
+                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+                                    <li class="nav-item"><a class="nav-link" href="blog-single.html">Blog Details</a></li>
+                                </ul>
+                            </li> 
+                            <li class="nav-item"><a class="nav-link" href="elements.html">Elemests</a></li>
+                            <li class="nav-item"><a class="nav-link" href="contact.html">Contact</a></li>
+                        </ul>
+                    </div> 
+                </nav>
+            </div>
+        </header>
+
+
+{/* 
+script */}
+        <script src="js/jquery-3.2.1.min.js"></script>
+        <script src="js/popper.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+        <script src="js/jquery.ajaxchimp.min.js"></script>
+        <script src="vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.js"></script>
+        <script src="vendors/nice-select/js/jquery.nice-select.js"></script>
+        <script src="js/mail-script.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/imagesloaded/imagesloaded.pkgd.min.js"></script>
+        <script src="vendors/isotope/isotope-min.js"></script>
+        <script src="js/stellar.js"></script>
+        <script src="vendors/lightbox/simpleLightbox.min.js"></script>
+        <script src="js/custom.js"></script>
+
+    
+    </> );
+}
+ 
+export default ViewRoom;
+
+
+        
+        
+

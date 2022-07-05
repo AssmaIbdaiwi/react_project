4afc72433a827adbfe9f1d721ff29ef2918c34cf
khaled94 <khaledbaniamer23@gmail.com>
2022-07-05 22:03:36 +0200

    main

--- booking_hotel/src/index.js
+++ booking_hotel/src/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
+
 import App from './App';
 
-=======
-// import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';  
->>>>>>> c784e4b267857e62f09976062e5bc6c7ddea2d9c
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -16,8 +11,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-<<<<<<< HEAD
-=======
-
-
->>>>>>> c784e4b267857e62f09976062e5bc6c7ddea2d9c

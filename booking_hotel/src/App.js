c784e4b267857e62f09976062e5bc6c7ddea2d9c
AssmaIbdaiwi <100359495+AssmaIbdaiwi@users.noreply.github.com>
2022-07-05 20:14:23 +0300

    main

--- booking_hotel/src/App.js
+++ booking_hotel/src/App.js
@@ -3,7 +3,7 @@ import './style/vendors/linericon/style.css';
 import './style/css/font-awesome.min.css';
 import './style/vendors/bootstrap-datepicker/bootstrap-datetimepicker.min.css';
 import './style/vendors/nice-select/css/nice-select.css';
-// import '../../style/vendors/owl-carousel/owl.carousel.min.css';
+import './style/vendors/owl-carousel/owl.carousel.min.css';
 import './style/vendors/lightbox/simpleLightbox.css';
 import './style/css/style.css';
 import './style/css/responsive.css';

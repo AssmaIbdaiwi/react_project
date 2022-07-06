e2c5bdc0ad1d00a3e5947960f830fdfea696169d
khaled94 <khaledbaniamer23@gmail.com>
2022-07-06 13:58:17 +0200

    main

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -1,18 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\RoomController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('apirooms',[RoomController::class , 'RoomAPI']);
+Route::post('addbook',[BookController::class , 'addBook']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

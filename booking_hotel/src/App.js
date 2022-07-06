fee795e01fd5c4eed72898b3eb0e13334c70c362
AhmadKhalaf <ahmad.alkhalafse@gmail.com>
2022-07-06 11:04:33 +0200

    main

--- booking_hotel/src/App.js
+++ booking_hotel/src/App.js
@@ -4,7 +4,7 @@ import {BrowserRouter , Routes , Route} from 'react-router-dom'
 import Rooms from "./components/Room";
 import Footer from "./layout/Footer";
 import Navbar from "./layout/Nav";
-
+import UserProfile from "./components/UserProfile";
 
 function App() {
 

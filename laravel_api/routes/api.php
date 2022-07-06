ae3511e6614b87ec918bf23370d7360a4ea7263a
Sujood-Mohammad <sujoodmoh1996@gmail.com>
2022-07-06 19:33:45 +0300

    api register

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -3,15 +3,17 @@
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\ContactController;
 use App\Http\Controllers\RoomController;
+use App\Http\Controllers\UserController;
 use App\Models\Contact;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('apirooms',[RoomController::class , 'RoomAPI']);
 Route::post('addbook',[BookController::class , 'addBook']);
 Route::get('contact',[ContactController::class , 'ContactAPI']);
-
-
+Route::post('register', [UserController::class, 'registerAPI']);
+Route::post('login', [UserController::class, 'loginAPI']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });

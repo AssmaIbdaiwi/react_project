bd7919ef8cb496cfa42a9b06ea0f7a9ffca6ab37
safa954 <100359594+safa954@users.noreply.github.com>
2022-07-06 08:46:43 -0700

    main

--- booking_hotel/src/App.js
+++ booking_hotel/src/App.js
@@ -6,16 +6,26 @@ import Footer from "./layout/Footer";
 import Navbar from "./layout/Nav";
 import UserProfile from "./components/UserProfile";
 import Home from './components/Home';
+import Register from './components/login/Register';
+import Login from "./components/login/Login";
+import About from './components/About';
+import Contact from './components/Contact';
+
+
 
 function App() {
 
   return (
-    <BrowserRouter >
+    <BrowserRouter>
       <Navbar />
-        <Routes>
-          <Route path='/rooms' element={<Rooms/>} />
-          <Route path='/' element={<Home/>} />
-        </Routes>
+      <Routes>
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );

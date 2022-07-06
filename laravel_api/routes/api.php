56e705fa721ccc47101475c45b924a1fc95f2fc7
AssmaIbdaiwi <100359495+AssmaIbdaiwi@users.noreply.github.com>
2022-07-06 15:02:28 +0300

    main

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ContactController;
 use App\Http\Controllers\RoomController;
+use App\Models\Contact;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('apirooms',[RoomController::class , 'RoomAPI']);
 Route::post('addbook',[BookController::class , 'addBook']);
+Route::get('contact',[ContactController::class , 'ContactAPI']);
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {

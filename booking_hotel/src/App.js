f78bc3ae0bbebee8ea675e53946495e815beb816
AssmaIbdaiwi <100359495+AssmaIbdaiwi@users.noreply.github.com>
2022-07-07 10:47:34 +0300

    main

--- booking_hotel/src/App.js
+++ booking_hotel/src/App.js
@@ -10,28 +10,40 @@ import Register from './components/login/Register';
 import Login from "./components/login/Login";
 import About from './components/About';
 import Contact from './components/Contact';
+import { createContext, useState,useEffect } from 'react';
 
-
+  export const userContext = createContext();
 
 function App() {
 
+  const [userData , setUserData ]= useState([]);
+
+  useEffect(() => {
+    
+    setUserData(JSON.parse(localStorage.getItem('user')));
+
+  }, [])
+  
+  console.log(userData)
   return (
     <BrowserRouter>
-      <Navbar />
+      <userContext.Provider value={userData}>
+        <Navbar />
 
-      <Routes>
-        <Route path="/rooms" element={<Rooms />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login /> } />
-        {/* if (path="/login" | path="/register"){
+        <Routes>
+          <Route path="/rooms" element={<Rooms />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          {/* if (path="/login" | path="/register"){
         }console.log(path) */}
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/ViewRoom/:id" element={<ViewRoom />} />
-        <Route path="/UserProfile" element={<UserProfile />} />
-      </Routes>
-      <Footer />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/ViewRoom/:id" element={<ViewRoom />} />
+          <Route path="/UserProfile" element={<UserProfile />} />
+        </Routes>
+        <Footer />
+      </userContext.Provider>
     </BrowserRouter>
   );
 
